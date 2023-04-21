612adc5d1aa8c591eb6281dbbd98fce9c2bd108d
Graham Leach-Krouse <gleachkr@gmail.com>
2023-04-21 18:02:42 -0500

    Initial steps on lawvere proof

--- Lawvere.lean
+++ Lawvere.lean
@@ -2,8 +2,6 @@ import Mathlib.CategoryTheory.Category.Basic
 import Mathlib.CategoryTheory.Limits.Shapes.FiniteProducts
 import Mathlib.CategoryTheory.Closed.Monoidal
 
-def icongruent [CategoryTheory.Category α] {A B C : α}  (i : B ⟶ C) (f : A ⟶ B) (g : A ⟶ B) := f ≫ i = g ≫ i
-
 noncomputable instance cartesian [CategoryTheory.Category α] [CategoryTheory.Limits.HasFiniteProducts α] : CategoryTheory.MonoidalCategory α where
   tensorObj x y := CategoryTheory.Limits.prod x y
   tensorUnit' := CategoryTheory.Limits.terminal α
@@ -15,16 +13,23 @@ noncomputable instance cartesian [CategoryTheory.Category α] [CategoryTheory.Li
   leftUnitor X := CategoryTheory.Limits.prod.leftUnitor X
   associator X Y Z := CategoryTheory.Limits.prod.associator X Y Z
 
-structure interpreter [CategoryTheory.Category α] [CategoryTheory.Limits.HasFiniteProducts α] [CategoryTheory.MonoidalClosed α] 
+structure interpreter [CategoryTheory.Category α] [CategoryTheory.Limits.HasFiniteProducts α] [mc : CategoryTheory.MonoidalClosed α] 
   (A E Y : α) (i : E ⟶ Y)  where
   map : A ⟶ (A ⟶[α] E)
   interprets : ∀f : A ⟶ E, ∃c : cartesian.tensorUnit' ⟶ A, 
-    have lhs := (cartesian.rightUnitor A).inv ≫ CategoryTheory.MonoidalClosed.uncurry (c ≫  map) ≫ i
-    have rhs := f ≫ i
+    let lhs := (cartesian.rightUnitor A).inv ≫ CategoryTheory.MonoidalClosed.uncurry (c ≫  map) ≫ i
+    let rhs := f ≫ i
     lhs = rhs
 
 def fixedPointProperty [CategoryTheory.Category α] [CategoryTheory.Limits.HasFiniteProducts α] { E Y : α } (i : E ⟶ Y) :=
   ∀t : E ⟶ E, ∃c: cartesian.tensorUnit' ⟶ E, c ≫  t ≫ i = c ≫  i
 
 theorem lawvereGeneralized [CategoryTheory.Category α] [CategoryTheory.Limits.HasFiniteProducts α] [CategoryTheory.MonoidalClosed α] {A E Y : α} {i : E ⟶ Y}
-  : Nonempty (interpreter A E Y i) → fixedPointProperty i := sorry
+  : Nonempty (interpreter A E Y i) → fixedPointProperty i := by
+    intros 
+    have ⟨int⟩ := ‹Nonempty (interpreter A E Y i)›
+    clear ‹Nonempty (interpreter A E Y i)›
+    let g := (CategoryTheory.Limits.prod.map (𝟙 A) int.map) ≫ (CategoryTheory.ihom.ev A).app E
+    simp at g
+    sorry
+
