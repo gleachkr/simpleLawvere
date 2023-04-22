dd15ff32c1a0acfc8924cb1527c941987d5f8cb4
Graham Leach-Krouse <gleachkr@gmail.com>
2023-04-22 12:23:14 -0500

    Mostly reduce to central equational lemma

--- Lawvere.lean
+++ Lawvere.lean
@@ -2,34 +2,44 @@ import Mathlib.CategoryTheory.Category.Basic
 import Mathlib.CategoryTheory.Limits.Shapes.FiniteProducts
 import Mathlib.CategoryTheory.Closed.Monoidal
 
-noncomputable instance cartesian [CategoryTheory.Category α] [CategoryTheory.Limits.HasFiniteProducts α] : CategoryTheory.MonoidalCategory α where
-  tensorObj x y := CategoryTheory.Limits.prod x y
-  tensorUnit' := CategoryTheory.Limits.terminal α
+open CategoryTheory
+
+noncomputable instance cartesian [Category α] [Limits.HasFiniteProducts α] : MonoidalCategory α where
+  tensorObj x y := Limits.prod x y
+  tensorUnit' := Limits.terminal α
   tensorHom := by
     intros X Y Z W f g
     simp
-    exact CategoryTheory.Limits.prod.lift (CategoryTheory.Limits.prod.fst ≫ f) (CategoryTheory.Limits.prod.snd ≫ g)
-  rightUnitor X := CategoryTheory.Limits.prod.rightUnitor X
-  leftUnitor X := CategoryTheory.Limits.prod.leftUnitor X
-  associator X Y Z := CategoryTheory.Limits.prod.associator X Y Z
+    exact Limits.prod.lift (Limits.prod.fst ≫ f) (Limits.prod.snd ≫ g)
+  rightUnitor X := Limits.prod.rightUnitor X
+  leftUnitor X := Limits.prod.leftUnitor X
+  associator X Y Z := Limits.prod.associator X Y Z
 
-structure interpreter [CategoryTheory.Category α] [CategoryTheory.Limits.HasFiniteProducts α] [mc : CategoryTheory.MonoidalClosed α] 
+structure interpreter [Category α] [Limits.HasFiniteProducts α] [mc : MonoidalClosed α] 
   (A E Y : α) (i : E ⟶ Y)  where
   map : A ⟶ (A ⟶[α] E)
   interprets : ∀f : A ⟶ E, ∃c : cartesian.tensorUnit' ⟶ A, 
-    let lhs := (cartesian.rightUnitor A).inv ≫ CategoryTheory.MonoidalClosed.uncurry (c ≫  map) ≫ i
+    let lhs := (cartesian.rightUnitor A).inv ≫ MonoidalClosed.uncurry (c ≫  map) ≫ i
     let rhs := f ≫ i
     lhs = rhs
 
-def fixedPointProperty [CategoryTheory.Category α] [CategoryTheory.Limits.HasFiniteProducts α] { E Y : α } (i : E ⟶ Y) :=
+def fixedPointProperty [Category α] [Limits.HasFiniteProducts α] { E Y : α } (i : E ⟶ Y) :=
   ∀t : E ⟶ E, ∃c: cartesian.tensorUnit' ⟶ E, c ≫  t ≫ i = c ≫  i
 
-theorem lawvereGeneralized [CategoryTheory.Category α] [CategoryTheory.Limits.HasFiniteProducts α] [CategoryTheory.MonoidalClosed α] {A E Y : α} {i : E ⟶ Y}
+theorem lawvereGeneralized [Category α] [Limits.HasFiniteProducts α] [MonoidalClosed α] {A E Y : α} {i : E ⟶ Y}
   : Nonempty (interpreter A E Y i) → fixedPointProperty i := by
     intros 
     have ⟨int⟩ := ‹Nonempty (interpreter A E Y i)›
     clear ‹Nonempty (interpreter A E Y i)›
-    let g := (CategoryTheory.Limits.prod.map (𝟙 A) int.map) ≫ (CategoryTheory.ihom.ev A).app E
+    let g := (Limits.prod.map (𝟙 A) int.map) ≫ (ihom.ev A).app E
     simp at g
-    sorry
-
+    have fact : ∀f : A ⟶ E, ∃c : cartesian.tensorUnit' ⟶ A, ∀a : cartesian.tensorUnit' ⟶ A, 
+      (Limits.prod.lift c a) ≫ g ≫ i = a ≫ f ≫ i := sorry
+    intros t
+    let k : A ⟶ E := Limits.diag A ≫ g ≫ t
+    have ⟨k',eq₁⟩ := fact k
+    have eq₂ := eq₁ k'
+    have rweq : Limits.prod.lift k' (k' ≫ int.map) = k' ≫ Limits.prod.lift (𝟙 A) int.map := by simp
+    simp at eq₂ 
+    rw [rweq] at eq₂
+    refine ⟨Limits.prod.lift k' (k' ≫ int.map) ≫  (ihom.ev A).app E, ?_⟩
